94b3f3b06b00969035f0c56ca483e9923d4707fa
MalgaroJr <67379587+MalgaroJr@users.noreply.github.com>
2021-04-20 18:18:26 -0300

    added chi cuadrado test

--- 2.1/2-1.py
+++ 2.1/2-1.py
@@ -20,6 +20,7 @@ def generador_pmc(seed, n):
         x = completar_ceros(x)
         seed = int(x[2:6])
         poblacion.append(seed)
+    poblacion = normalizar(poblacion)
     return poblacion
 
 
@@ -27,12 +28,13 @@ def generador_gcl(seed, n):
     poblacion = [(a * seed + c) % m]
     for i in range(1, n):
         poblacion.append((a * poblacion[i-1] + c) % m)
+    poblacion = normalizar(poblacion)
     return poblacion
 
 
-def plot_pmc():
-    x = generador_pmc(1234, 50)
-    y = generador_pmc(5678, 50)
+def plot_pmc(seed1, seed2, n):
+    x = generador_pmc(seed1, n)
+    y = generador_pmc(seed2, n)
 
     sns.regplot(x=x, y=y, color='black', scatter_kws={'alpha': 0.4}, lowess=True)
     plt.xticks(())
@@ -40,15 +42,53 @@ def plot_pmc():
     plt.show()
 
 
-def plot_gcl():
-    x = generador_gcl(9999, 1000)
-    y = generador_gcl(8888, 1000)
-
+def plot_gcl(seed1, seed2, n):
+    x = generador_gcl(seed1, n)
+    y = generador_gcl(seed2, n)
     sns.regplot(x=x, y=y, color='black', scatter_kws={'alpha': 0.4}, lowess=True)
     plt.xticks(())
     plt.yticks(())
     plt.show()
 
+def normalizar(a):
+    minimo = min(a)
+    maximo = max(a)
+    arreglo = []
+    for i in a:
+        valor = (i - minimo)/(maximo - minimo)
+        arreglo.append(valor)
+    return arreglo
 
+def chi_cuadrado(muestra):
+    cant_clases = 10
+    celdas = lambdas(muestra,cant_clases)
+    e = len(muestra)/cant_clases
+    chi = 0
+    for o in celdas:
+        chi += ((o-e)**2)/e
+    print("grado de libertad: " + str(cant_clases-1))
+    print("con un alfa de valor: 0.05")
+    print("el valor obtenido es: "+ str(chi))
+
+def lambdas(muestra, c):
+    min = 0
+    max = 1 / c
+    celdas = []
+    x = sum(map(lambda x: min <= x < max, muestra))
+    celdas.append(x)
+    for x in range(1, c):
+        min += 1/c
+        max += 1/c
+        x = sum(map(lambda x: min < x <= max, muestra))
+        celdas.append(x)
+    celdas[9] += 1
+    return celdas
+
+x = generador_gcl(9999, 10000)
+chi_cuadrado(x)
+
+
+"""
 plot_pmc()
-plot_gcl()
+plot_gcl()"""
+
