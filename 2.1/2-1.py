4e12d5a33ada86d608489b60e03ae0a9d62df5a1
MalgaroJr <67379587+MalgaroJr@users.noreply.github.com>
2021-04-21 15:30:03 -0300

    fixes

--- 2.1/2-1.py
+++ 2.1/2-1.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from decimal import Decimal
 import numpy as np
-
+import pandas as pd
 
 a = 25214903917
 c = 11
 m = 2 ** 48
+
+
 
 
 def completar_ceros(x):
@@ -14,7 +16,6 @@ def completar_ceros(x):
     for _ in range(0, (8-len(x))):
         x = '0' + x
     return x
-
 
 def generador_pmc(seed, n):
     poblacion = [seed]
@@ -26,14 +27,12 @@ def generador_pmc(seed, n):
     poblacion = normalizar(poblacion)
     return poblacion
 
-
 def generador_gcl(seed, n):
     poblacion = [(a * seed + c) % m]
     for i in range(1, n):
         poblacion.append((a * poblacion[i-1] + c) % m)
     poblacion = normalizar(poblacion)
     return poblacion
-
 
 def plot_pmc(seed1, seed2, n):
     x = generador_pmc(seed1, n)
@@ -44,7 +43,6 @@ def plot_pmc(seed1, seed2, n):
     plt.yticks(())
     plt.show()
 
-
 def plot_gcl(seed1, seed2, n):
     x = generador_gcl(seed1, n)
     y = generador_gcl(seed2, n)
@@ -53,7 +51,6 @@ def plot_gcl(seed1, seed2, n):
     plt.xticks(())
     plt.yticks(())
     plt.show()
-
 
 def normalizar(a):
     minimo = min(a)
@@ -64,22 +61,31 @@ def normalizar(a):
         arreglo.append(valor)
     return arreglo
 
-
 def chi_cuadrado(muestra):
+    df_chi = pd.read_excel("D:\GitHub\Trabajos-Simulacion\chi-cuadrado.xlsx")
+    df_chi = pd.DataFrame(df_chi)
     cant_celdas = 10
+    alpha = 0.05
+    row = df_chi[df_chi[0] == cant_celdas - 1]
+    valor = float(row[alpha])
     celdas = contar_observ(muestra, cant_celdas)
     e = len(muestra)/cant_celdas
     chi = 0
     for o in celdas:
         chi += ((o - e) ** 2)/e
-    print("grado de libertad: " + str(cant_celdas - 1))
-    print("con un alfa de valor: 0.05")
+    if chi < valor:
+        print("La hipotesis nula es aceptada porque la prueba es menor que el valor crítico")
+    else:
+        print("la hipotesis nula no es aceptada porque la prueba dio mayor al valor crítico")
     print("el valor obtenido es: " + str(chi))
+    print("el valor critico con alpha= " + str(alpha) + " y grado de libertad= " + str(
+        cant_celdas - 1) + " es el siguiente --> " + str(valor))
+    print(str(chi) + '<' + str(valor))
+
     x = ['Clase 1','Clase 2','Clase 3','Clase 4','Clase 5','Clase 6','Clase 7', 'Clase 8','Clase 9','Clase 10']
     # arregalr que no sea hardcodeado
     plt.bar(x,celdas)
     plt.show()
-
 
 def contar_observ(muestra, cant_celdas):
     step = Decimal(str(1/cant_celdas))
@@ -124,17 +130,32 @@ def runs_above_below(muestra):
     plt.show()
 
 def reverse_arrangements(muestra):
+    df_reverse = pd.read_excel("D:\GitHub\Trabajos-Simulacion\critical-arrangement.xlsx")
+    df_reverse = pd.DataFrame(df_reverse)
+    alpha = 0.025
+    n = 100
     cont = 0
-    for i in range(0,99):
-        for j in range(i+1, 100):
+    row = df_reverse[df_reverse[0] == n]
+    min = int(row[(1 - alpha)])
+    max = int(row[alpha])
+    for i in range(0,n-1):
+        for j in range(i+1, n):
             if muestra[i] > muestra[j]:
                 cont += 1
-    print(cont)
+
+    if (min < cont) and (cont <= max):
+        print("La hipótesis nula es aceptada porque nuestro valor está dentro de los parámetros")
+    else:
+        print("la hipótesis no es aceptada porque no cumple con los parámetros")
+    print(str(min) + '<' + str(cont) + '<=' + str(max))
+
 
 x = generador_gcl(9999, 1000)
-#chi_cuadrado(x)
-#runs_above_below(x)
+chi_cuadrado(x)
+runs_above_below(x)
 reverse_arrangements(x)
+
+
 
 
 """
