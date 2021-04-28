3c4acd38fe7c7e746e7bf972e715af420e7d04fd
MalgaroJr <67379587+MalgaroJr@users.noreply.github.com>
2021-04-28 11:25:05 -0300

    fixes

--- 2.2/2.2.py
+++ 2.2/2.2.py
@@ -4,104 +4,92 @@ import numpy as np
 import scipy.stats as ss
 
 
-def dist_uni(a, b, r):
+def dist_uni(a, b):
+    r = random.random()
     x = a + (b - a) * r
     return x
 
-def dist_exp(media, r):
+def dist_exp(media):
+    r = random.random()
     x = - media * np.log(r)
     return x
 
-def dist_gamma(media, r, a, k):
-    x = 0
+def dist_gamma(media, var):
+    k = int((media**2)/var)
+    a = media/var
+    r = np.random.random(k)
     if media % 2 == 0:
-        for i in range(0,k):
-            x += (-1/a) * np.log(r[i])
+        ri = np.prod(r)
+        x = (-1/a) * np.log(ri)
     elif k==1:
         x = dist_exp(media, r[0])
     else:
-        ki = 0
-        for i in range(0,k):
-            ki += r[i]
+        ki = sum(r)
         z = (ki - k/2) / np.sqrt(k/12)
-        for i in range(0,k):
-            x += (-1/a) * np.log(r[i])
-        x += z ** 2
+        ri = np.prod(r)
+        x = (-1 / a) * np.log(ri) + z**2
     return x
 
-def dist_norm(mu, sigma, k, r):
-    ki = 0
-    for i in range(0,k):
-        ki += r[i]
-    x = (sigma * ((12/k) ** (1/2)) ) * (ki- k/2) + mu
-
+def dist_norm(mu, sigma, k):
+    r = np.random.random(k)
+    ki = sum(r)
+    x = (sigma * (np.sqrt(12/k)) * (ki- k/2)) + mu
     return x
 
-def dist_chi2(a, m, k, r):
-    x = 0
-    if m % 2 == 0:
-        for i in range(0,k):
-            x += (-1/a) * np.log(r[i])
-    elif m < 30:
-        ki = 0
-        for i in range(0, k):
-            ki += (r[i] - k / 2)
-        z = ki / np.sqrt(k / 12)
-        for i in range(0, k):
-            x += (-1 / a) * np.log(r[i])
-        x += z ** 2
-    else:
-        chi = ss.chi2(1 - a, m)
-        z = np.sqrt(chi) - np.sqrt(2 * m) - 1
-        x = ((z + np.sqrt(2 * m - 1)) ** 2) / 2
-    return x
-
-def dist_logn(mu, sigma, k, r):
-    z = 0
-    for i in range(0, k):
-        z += (r[i] - (k / 2))
-    x = np.exp(mu + sigma * ((k/12) ** (-1/2)) * z)
-    return x
-
-def dist_geom(r, p):
-    q = 1 - p
-    x = math.log10(r)/math.log10(q)
-    return int(x)
-
-def dist_pascal(mu, sigma, r):
+def dist_pascal(mu, sigma):
     p = mu/sigma
-    k = (mu ** 2)/(sigma - mu)
-    ki = 0
-    if type(k) == int:
-        for i in range(0, k):
-            ki += math.log10(r[i])
-        x = ki / math.log10(1 - p)
+    k = float((mu ** 2)/(sigma - mu))
+    if not(k.is_integer()) and random.random() <= .5:
+        k = int(k)
     else:
-        x = 0 # no entiendos
+        k = int(k) + 1
+    k = int(k)
+    r = np.random.random(k)
+    ki = np.prod(r)
+    x = np.log(ki) / np.log(1 - p)
     return x
 
-def dist_binom(mu, sigma, r):
+def dist_binom(mu, sigma):
     p = (mu - sigma)/mu
     n = int((mu ** 2)/(mu - sigma))
+    r = np.random.random(n)
     x = 0
     for i in range(0, n):
         if r[i] <= p:
             x += 1
     return x
 
-def dist_hiperg(Ne, n, p, r):
+def dis_binom2(p,n):
+    r = np.random.random(n)
     x = 0
+    for i in range(0, n):
+        if r[i] <= p:
+            x += 1
+    return x
+
+def dist_hiperg(Ne, n, p):
+    x = 0
+    r = np.random.random(n)
     for i in range(1, n):
-        if r[i-1] < p:
+        if r[i-1] <= p:
             s = 1
             x += 1
         else:
             s = 0
         p = ((Ne * p) - s) / (Ne - 1)
         Ne -= 1
-    return 0
+    return x
 
-def dist_poisson():
-    #same
-    return 0
-
+def dist_poisson(l):
+    x = 0
+    b = np.exp(-l)
+    ri = random.random()
+    while ri > b:
+        x += 1
+        ri *= random.random()
+    return x
+x = []
+for i in range(0,23):
+    x.append(dist_poisson(5))
+print(x)
+print(np.random.poisson(5,23))
