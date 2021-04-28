2381587a088f14c5a55c649334f50ae4a50e8d26
MalgaroJr <67379587+MalgaroJr@users.noreply.github.com>
2021-04-28 20:41:24 -0300

    more fixes

--- 2.2/2.2.py
+++ 2.2/2.2.py
@@ -2,6 +2,8 @@ import math
 import random
 import numpy as np
 import scipy.stats as ss
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def dist_uni(a, b):
@@ -22,7 +24,7 @@ def dist_gamma(media, var):
         ri = np.prod(r)
         x = (-1/a) * np.log(ri)
     elif k==1:
-        x = dist_exp(media, r[0])
+        x = dist_exp(media)
     else:
         ki = sum(r)
         z = (ki - k/2) / np.sqrt(k/12)
@@ -36,18 +38,6 @@ def dist_norm(mu, sigma, k):
     x = (sigma * (np.sqrt(12/k)) * (ki- k/2)) + mu
     return x
 
-def dist_pascal(mu, sigma):
-    p = mu/sigma
-    k = float((mu ** 2)/(sigma - mu))
-    if not(k.is_integer()) and random.random() <= .5:
-        k = int(k)
-    else:
-        k = int(k) + 1
-    k = int(k)
-    r = np.random.random(k)
-    ki = np.prod(r)
-    x = np.log(ki) / np.log(1 - p)
-    return x
 
 def dist_binom(mu, sigma):
     p = (mu - sigma)/mu
@@ -59,7 +49,7 @@ def dist_binom(mu, sigma):
             x += 1
     return x
 
-def dis_binom2(p,n):
+def dis_binom2(n, p):
     r = np.random.random(n)
     x = 0
     for i in range(0, n):
@@ -88,8 +78,115 @@ def dist_poisson(l):
         x += 1
         ri *= random.random()
     return x
-x = []
-for i in range(0,23):
-    x.append(dist_poisson(5))
-print(x)
-print(np.random.poisson(5,23))
+
+def dist_disc():
+    return 0
+
+def prueba_uni():
+    uni_py = np.random.uniform(0,50,10000)
+    uni_sus = []
+    for i in range(10000):
+        uni_sus.append(dist_uni(0,50))
+    ax = sns.histplot(data=uni_py,
+                      binwidth=0.5,
+                      color='blue',
+                      alpha=0.5,
+                      label='Generador Python')
+    ab = sns.histplot(data=uni_sus,
+                      binwidth=0.5,
+                      color='red',
+                      alpha=0.5,
+                      label='Generado por nuestro código')
+    ax.set(xlabel='Distribución Uniforme', ylabel='Frecuencia')
+    plt.legend()
+    plt.show()
+
+def prueba_exp():
+    exp_py = np.random.exponential(1/10,10000)
+    exp_sus = []
+    for i in range(0,10000):
+        exp_sus.append(dist_exp(1/10))
+    ax = sns.histplot(data= exp_py,
+                      binwidth=0.005,
+                      color='blue',
+                      alpha=0.5,
+                      label='Generador Python')
+    ab = sns.histplot(data= exp_sus,
+                      binwidth=0.005,
+                      color='red',
+                      alpha=0.5,
+                      label= 'Generado por nuestro código')
+    ax.set(xlabel='Distribución Exponencial', ylabel='Frecuencia')
+    plt.legend()
+    plt.show()
+
+def prueba_normal():
+    norm_py = ss.norm.rvs(loc=10, scale=3, size=10000 )
+    norm_sus = []
+    for i in range(10000):
+        norm_sus.append(dist_norm(10,3,12))
+    ax = sns.histplot(data=norm_py,
+                      binwidth=0.1,
+                      color='blue',
+                      alpha=0.5,
+                      label='Generador Python')
+    ab = sns.histplot(data=norm_sus,
+                      binwidth=0.1,
+                      color='red',
+                      alpha=0.5,
+                      label='Generado por nuestro código')
+    ax.set(xlabel='Distribución Normal', ylabel='Frecuencia')
+    plt.legend()
+    plt.show()
+
+def prueba_bin():
+    bin_py = np.random.binomial(50,0.10,10000)
+    bin_sus = []
+    for i in range(10000):
+        bin_sus.append(dis_binom2(50, 0.1))
+    x = range(0,len(np.bincount(bin_py)))
+    plt.bar(x,np.bincount(bin_py),
+             alpha=0.5,
+             color='blue',
+             label='Generador Python'
+    )
+    x = range(0, len(np.bincount(bin_sus)))
+    plt.bar(x, np.bincount(bin_sus),
+             alpha=0.5,
+             color='red',
+             label='Generado por nuestro código'
+             )
+    plt.xlabel('Distribución Binomial')
+    plt.ylabel('Frecuencia')
+    plt.legend()
+    plt.show()
+
+def prueba_poisson():
+    poisson_py = ss.poisson.rvs(mu=5, size=10000)
+    poisson_sus = []
+    for i in range(10000):
+        poisson_sus.append(dist_poisson(5))
+    x = range(0, len(np.bincount(poisson_py)))
+    plt.bar(x, np.bincount(poisson_py),
+            alpha=0.5,
+            color='blue',
+            label='Generador Python'
+            )
+    x = range(0, len(np.bincount(poisson_sus)))
+    plt.bar(x, np.bincount(poisson_sus),
+            alpha=0.5,
+            color='red',
+            label='Generado por nuestro código'
+            )
+    plt.xlabel('Distribución de Poisson')
+    plt.ylabel('Frecuencia')
+    plt.legend()
+    plt.show()
+
+
+
+prueba_uni()
+prueba_exp()
+prueba_normal()
+prueba_bin()
+prueba_poisson()
