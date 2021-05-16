bad258f91ab03bd5380ee5ce013095e2069cc4f7
Juan Ignacio Elias <juanignacioelias@hotmail.com>
2021-05-16 16:58:40 -0300

    Error en np.exp, era np.random.exponential (la distribucion)

--- 3.1/3.1.py
+++ 3.1/3.1.py
@@ -6,9 +6,10 @@ import os
 amount = bigs = initial_inv_level = inv_level = next_event_type = num_events = num_months = num_value_demand = smalls = 0
 a_holding = a_shortage = holding_cost = incremental_cost = maxlag = mean_interdemand = minlag = setup_cost = shortage_cost = sim_time = time_last_event = total_ordering_cost = 0.0
 prob_distrib_demand = [0.0] * 26
-time_next_event = [0.0] *5
+time_next_event = [0.0] * 5
 
-def timing():       #siempre entra en el evento tipo 2
+
+def timing():       # Siempre entra en el evento tipo 2
     global next_event_type, sim_time, num_events
     min_time_next_event = 10**29
     next_event_type = 0
@@ -17,14 +18,15 @@ def timing():       #siempre entra en el evento tipo 2
             min_time_next_event = time_next_event[i]
             next_event_type = i
     if next_event_type == 0:
-        #lista de eventos vacia, se detiene la simulacion
+        # Lista de eventos vacía, se detiene la simulación
         print(colored("lista de eventos vacía en el tiempo: ", "red") + str(sim_time))
-        #os._exit(1)
+        # os._exit(1)
     sim_time = min_time_next_event
+
 
 def initialize():
     global sim_time, inv_level, total_ordering_cost, a_holding, a_shortage, time_last_event, time_next_event
-    sim_time = 0                                # inicializa el reloj de simulacion
+    sim_time = 0                                # inicializa el reloj de simulación
 
     # inicializa las variables de estado
     inv_level = initial_inv_level
@@ -34,36 +36,41 @@ def initialize():
 
     # inicializa la lista de eventos
     time_next_event[1] = 10 ** 30
-    time_next_event[2] = sim_time + np.exp(mean_interdemand)
+    time_next_event[2] = sim_time + np.random.exponential(mean_interdemand)
     time_next_event[3] = num_months
     time_next_event[4] = 0.0
+
 
 def order_arrival():
     global inv_level, time_next_event, amount
     inv_level += amount
-    time_next_event[1] = 10**30     #ya que no va a llegar otro pedido se saca este evento de la lista
+    time_next_event[1] = 10**30     # Ya que no va a llegar otro pedido se saca este evento de la lista
+
 
 def random_integer(prob):
-    u = np.random.random()          #genera un número con dist uniforme entre (0, 1)
+    u = np.random.random()          # Genera un número con dist uniforme entre (0, 1)
     i = 1
     while u >= prob[i]:
         i += 1
     return i
 
+
 def demand():
     global inv_level, time_next_event, mean_interdemand, prob_distrib_demand
     inv_level -= random_integer(prob_distrib_demand)
-    time_next_event[2] = sim_time + np.exp(mean_interdemand)
+    time_next_event[2] = sim_time + np.random.exponential(mean_interdemand)
+
 
 def evaluate():
     global inv_level, time_next_event, amount, total_ordering_cost, smalls, bigs
-    if inv_level < smalls:                                            #evalua si el stock es menor al minimo
+    if inv_level < smalls:                                          # evalúa si el stock es menor al minimo
         #se realiza un pedido
         amount = bigs - inv_level
         total_ordering_cost += setup_cost + incremental_cost * amount
         #se pone en la lista de eventos el pedido
         time_next_event[1] = sim_time + np.random.uniform(minlag, maxlag)
-    time_next_event[4] = sim_time + 1                                 #se pone en la lista de eventos la sig evaluacion
+    time_next_event[4] = sim_time + 1                               # se pone en la lista de eventos la sig evaluación
+
 
 def report():
     global holding_cost, total_ordering_cost, num_months, shortage_cost, a_shortage, a_shortage
@@ -73,20 +80,21 @@ def report():
     tot = avg_shortage_cost + avg_holding_cost + avg_ordering_cost
     print("("+str(smalls)+", "+str(bigs)+")     "+str(tot)+"    "+str(avg_ordering_cost)+"  "+str(avg_holding_cost)+"   "+str(avg_shortage_cost))
 
+
 def update_time_avg_stats():
     global time_last_event, a_shortage, a_holding
-    #determian el tiempo desde el último evento y actualiza el tiempo del último evento al tiempo actual
+    # Determian el tiempo desde el último evento y actualiza el tiempo del último evento al tiempo actual
     time_since_last_event = sim_time - time_last_event
     time_last_event = sim_time
 
-    #determina el estado del nivel de inventario durante el intervalo previo
+    # Determina el estado del nivel de inventario durante el intervalo previo
     if inv_level < 0:
         a_shortage -= inv_level * time_since_last_event
     elif inv_level > 0:
         a_holding += inv_level * time_since_last_event
 
 
-num_events = 4      #número de eventos para la función del tiempo
+num_events = 4      # Número de eventos para la función del tiempo
 initial_inv_level = 60
 num_months = 120
 num_policies = 9
@@ -98,7 +106,7 @@ holding_cost = 1
 shortage_cost = 5
 minlag = 0.5
 maxlag = 1
-prob_distrib_demand = [0 , 1/6 , 3/6, 5/6, 1]
+prob_distrib_demand = [0, 1/6, 3/6, 5/6, 1]
 small = [0, 20, 20, 20, 20, 40, 40, 40, 60, 60]
 big = [0, 40, 60, 80, 100, 60, 80, 100, 80, 100]
 for i in range(1, num_policies):
